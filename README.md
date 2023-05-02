9d0cff36b7a1b46203989af2d0628a5e4ffb6f92
Bryanset <48760390+Bryanset@users.noreply.github.com>
2023-05-02 20:47:44 +0200

    Added layout declaration / Button functions / class declaration

---
+++ README.md
@@ -0,0 +1,11 @@
+# Texted_App
+Texted_Project
+
+Welcome to my first Android app! 
+In this project I have created a contact book using the Firebase platform to store contact data.
+
+App Features
+
+- The app allows the user to add, edit and delete contacts.
+- Contact data is stored in a real-time Firebase database.
+- Users can authenticate using their Google credentials.
---
+++ app/src/main/java/com/example/texted_app/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.texted_app;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.texted_app.databinding.ActivityMainBinding;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button bloginButt, bregistButt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    bloginButt = findViewById(R.id.button_log_in);
+
+    bloginButt.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(MainActivity.this, contacts_class.class);
+            startActivity(intent);
+        }
+    });
+
+    }
+
+
+}
