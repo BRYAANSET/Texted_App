e95f5020678d4937f46a7b6f8e8f378e7e4268d0
bryan <bcn.gamerlol@gmail.com>
2023-05-04 01:50:12 +0200

    Added Current user Id to a label

--- app/src/main/java/com/example/texted_app/contacts_class.java
+++ app/src/main/java/com/example/texted_app/contacts_class.java
@@ -1,24 +1,52 @@
 package com.example.texted_app;
 
+import static android.content.ContentValues.TAG;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.rpc.context.AttributeContext;
 
 public class contacts_class extends AppCompatActivity {
 
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    FirebaseUser currentUser = mAuth.getCurrentUser();
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+
     // We declare two buttons
     Button bAddContButt, bLogOutButt;
+
+    //We create the label inputs
+   TextView showUserId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.contacts);
 
-        // We initialize the "Add Contact" button
         bAddContButt = findViewById(R.id.add_contact);
+        bLogOutButt = findViewById(R.id.log_out);
+        showUserId = findViewById(R.id.showUserId);
+
+        getUserId();
+
+        // We initialize the "Add Contact" button
         bAddContButt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -29,7 +57,6 @@ public class contacts_class extends AppCompatActivity {
         });
 
         // We initialize the "Log Out" button
-        bLogOutButt = findViewById(R.id.log_out);
         bLogOutButt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -39,6 +66,32 @@ public class contacts_class extends AppCompatActivity {
             }
         });
 
+    }
+
+    public void getUserId(){
+        showUserId = findViewById(R.id.showUserId);
+
+        DocumentReference docRef = db.collection("users").document(currentUser.getEmail());
+
+        docRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                if (documentSnapshot.exists()) {
+                    String username1= documentSnapshot.getString("fullName"); // Almacenamos el username en el array
+                    showUserId.setText(username1);
+                    Log.d(TAG, "Username: " + username1);
+
+                } else {
+                    Log.d(TAG, "No such document");
+                }
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d(TAG, "Error getting document: " + e.getMessage());
+            }
+        });
     }
 
 }
